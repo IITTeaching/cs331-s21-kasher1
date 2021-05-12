50126fde0f3f416ebe94bfecc52fedac2de2096a
krishasher <60676310+krishasher@users.noreply.github.com>
2021-05-12 13:19:04 -0500

    Update radix-sort.py

--- project/radix-sort.py
+++ project/radix-sort.py
@@ -8,8 +8,6 @@ def book_to_words(book_url='https://www.gutenberg.org/files/84/84-0.txt'):
 
 def radix_a_book(book_url='https://www.gutenberg.org/files/84/84-0.txt'):
     byteWords = book_to_words(book_url)
-    #Only does the first 100 words of the book
-    byteWords = byteWords[0:100]
     max = 0
     for i in range(1,len(byteWords)):
       if(len(byteWords[i])>len(byteWords[max])):
@@ -27,21 +25,23 @@ def sort(inputs, numIter):
         counts[x[y]]+=1
       except:
         counts[0]+=1
-        
+      
     for i in range(1,len(counts)):
       counts[i] += counts[i-1]
-    
+
     output = [None] * len(inputs)
+
     for i in inputs[::-1]:
       try:
         t = i[y]
       except:
         t = 0
       idx = counts[t]-1
-      while output[idx]!=None:
-        idx-=1
       output[idx] = i
+      counts[t]-=1
     inputs = output
   return inputs
 
 print(radix_a_book())
+
+
